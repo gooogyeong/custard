c50e00d4b3f04f335178503f698ee1a31cafacc9
Minkyung Lee <gooogyeong@gmail.com>
2020-06-26 21:32:55 +0900

    Refactor: deleted unncessary user functions

--- Custard-client/client/src/components/Login.js
+++ Custard-client/client/src/components/Login.js
@@ -2,30 +2,19 @@ import React, { Component } from "react";
 import firebase from "firebase/app";
 import { Link, Redirect, withRouter } from "react-router-dom";
 import { Button } from "@material-ui/core";
-// import { login } from "./UserFunctions";
-import GoogleLogin from "react-google-login";
-import { GOOGLE_CLIENT_ID } from "../config/googleClientId";
-import axios from "axios";
-//import google_logo from "../google_logo.png";
 import google_logo_2 from "../google_logo_2.png";
 import "../styles/Login.css";
-import { provider, UserRef, getUserRef } from "../firebase/index.ts";
-import { onSignInClick } from "../firebase";
+import { provider } from "../firebase/index.ts";
 
 class Login extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      //tempToken: "",
-      needSignUp: false,
-    };
     this.handleSignInSuccess = this.handleSignInSuccess.bind(this);
   }
 
   handleSignInSuccess(res) {
     //! on? once?
     this.props.checkIfRegistered(res.user.uid);
-    console.log(res.user.uid);
     // UserRef.orderByChild("uuid")
     //   .equalTo(res.user.uid)
     //   .once("value", (snap) => {
@@ -53,37 +42,19 @@ class Login extends Component {
 
   async handleSignInClick() {
     await firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL);
-    firebase
-      .auth()
-      .signInWithPopup(provider)
-      .then(this.handleSignInSuccess)
-      .catch(function (error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
-      });
+    firebase.auth().signInWithPopup(provider).then(this.handleSignInSuccess);
   }
-
-  handleSignUp() {}
 
   componentDidMount() {
     this.props.checkAuthPersistence();
   }
 
   render() {
-    // console.log(this.props.isLogin);
     if (this.props.isLogin) {
       return <Redirect to="/mypage" />; //!mypage
     }
     if (this.props.isSignUp) {
-      console.log("you gotta signup");
       this.props.history.push("/signup");
-      //return <Redirect to="/signup" />;
     }
     return (
       <div className="login" style={{ padding: "40px 0 0 0" }}>
